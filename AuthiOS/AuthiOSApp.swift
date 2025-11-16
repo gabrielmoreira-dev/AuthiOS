7a1569e7a8c6a9fda5e04dbc2de8647b5a3ce9a6
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-11-16 12:10:33 -0300

    Initial Commit

---
+++ AuthiOS/AuthiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthiOSApp.swift
+//  AuthiOS
+//
+//  Created by Gabriel on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
